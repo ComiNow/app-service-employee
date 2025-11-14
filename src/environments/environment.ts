52b4f78215ccefe99a94d3f16ee165799e453de6
Julian Rodriguez G <julian.rodriguezg@udea.edu.co>
2025-11-13 21:00:24 -0500

    Add AWS resources documentation and update deployment configurations for ComiNow employee frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'http://localhost:3000/api',
-  //baseUrl: 'https://s05phrls-3000.use.devtunnels.ms/api',
+  production: true,
+  baseUrl: 'https://z2j7v360-3000.use.devtunnels.ms/api',
 };
